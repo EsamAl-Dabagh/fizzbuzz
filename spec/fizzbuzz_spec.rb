88e50e2123e9ceeff51712069a553e4c644fffb4
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-07-25 12:18:55 +0100

    created functioning fizzbuzz_spec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,25 @@ describe "fizzbuzz" do
   it "returns 'fizz' when passed 3" do 
      expect(fizzbuzz(3)).to eq "fizz"
   end
+
+  it "returns 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+
+  it "returns 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it "returns 'fizz' when passed 9" do
+    expect(fizzbuzz(9)).to eq "fizz"
+  end
+
+  it "returns 8 when passed 8" do
+    expect(fizzbuzz(8)).to eq 8
+  end
+
+  it "returns 'fizzbuzz' when passed 6360" do
+    expect(fizzbuzz(6360)).to eq "fizzbuzz"
+  end
+
 end
